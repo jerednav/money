fdd213eef55402dc4a7603e620cf4553aa0afe4f
jerednav <80506833+jerednav@users.noreply.github.com>
2021-12-15 22:44:30 -0800

    setting up firebase auth

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
+import "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCTg9wad1GYUcHkhnv6NiCKFZAf2SGEO1o",
@@ -16,5 +17,6 @@ firebase.initializeApp(firebaseConfig);
 
 //init each service
 const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
 
-export { projectFirestore };
+export { projectFirestore, projectAuth };
