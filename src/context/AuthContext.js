ba859bd2752bd9251c7e51370ae5022c50a8771f
jerednav <80506833+jerednav@users.noreply.github.com>
2021-12-18 17:22:00 -0800

    Dispatch login action

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -4,6 +4,8 @@ export const AuthContext = createContext();
 
 export const authReducer = (state, action) => {
   switch (action.type) {
+    case "LOGIN":
+      return { ...state, user: action.payload };
     default:
       return state;
   }
@@ -13,8 +15,7 @@ export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, {
     user: null,
   });
-
-  dispatch({ type: "LOG_IN" });
+  console.log("AuthContext state:", state);
 
   return (
     <AuthContext.Provider value={{ ...state, dispatch }}>
