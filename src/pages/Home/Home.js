f6978c61c1f0fae80be9097be01824b59caa26aa
jerednav <80506833+jerednav@users.noreply.github.com>
2021-12-20 21:39:41 -0800

    ading new transaction form

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,15 @@
 import styles from "./Home.module.css";
 
-import React from "react";
+//components
+import TransactionForm from "./TransactionForm";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>transaction list</div>
+      <div className={styles.sidebar}>
+        <TransactionForm />
+      </div>
+    </div>
+  );
 }
