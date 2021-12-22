dc485d097260d36aa12cde6b48c187e1176bec90
jerednav <80506833+jerednav@users.noreply.github.com>
2021-12-21 21:37:12 -0800

    using usefirestore hook to connect do firebase db

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Redirect } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { useAuthContext } from "./hooks/useAuthContext";
 
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,19 @@
+import { useAuthContext } from "../../hooks/useAuthContext";
+
+//styles
 import styles from "./Home.module.css";
 
 //components
 import TransactionForm from "./TransactionForm";
 
 export default function Home() {
+  const { user } = useAuthContext();
+
   return (
     <div className={styles.container}>
       <div className={styles.content}>transaction list</div>
       <div className={styles.sidebar}>
-        <TransactionForm />
+        <TransactionForm uid={user.uid} />
       </div>
     </div>
   );
